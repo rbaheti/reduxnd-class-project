7459bae10db012a91c3e910b40c40bd1a973dde9
Rashmi <baheti.rash123@gmail.com>
2019-02-09 16:41:20 -0500

    adds a subscribe function which listens to the changes in the store.

--- index.js
+++ index.js
@@ -6,10 +6,23 @@ function createStore() {
   // 4. Update the state
 
   let state;
+  let listeners = [];
 
   const getState = () => state;
 
+  // subscribe is on of store's function.
+  // When called, it is passed a single function.
+  // And it returns a function.
+  // Note: subscribe does not modify internal state tree variable! 
+  const subscribe = (listener) => {
+    listeners.push(listener);
+    return () => {
+      listeners = listeners.filter(l => l !== listener);
+    }
+  }
+
   return {
-    getState
+    getState,
+    subscribe
   };
 }
