f3226f7bdfa235dc8320e4d98e9bffadcbe8318c
Rashmi <baheti.rash123@gmail.com>
2019-02-18 11:29:07 -0500

    seperates libraray function (store) and the app function (action)

--- index.js
+++ index.js
@@ -1,36 +1,40 @@
-// todos is a ruducer.
-function todos (state = [], action) {
-  if(action.type === "ADD_TODO") {
-    return state.concat([action.todo])
-  }
-  return state;
-}
-
-function createStore() {
+// Library Code
+function createStore (reducer) {
   // The store should have four parts
   // 1. The state
   // 2. Get the state.
   // 3. Listen to changes on the state.
   // 4. Update the state
 
-  let state;
-  let listeners = [];
+  let state
+  let listeners = []
 
-  const getState = () => state;
+  const getState = () => state
 
-  // subscribe is on of store's function.
-  // When called, it is passed a single function.
-  // And it returns a function.
-  // Note: subscribe does not modify internal state tree variable! 
   const subscribe = (listener) => {
-    listeners.push(listener);
+    listeners.push(listener)
     return () => {
-      listeners = listeners.filter(l => l !== listener);
+      listeners = listeners.filter((l) => l !== listener)
     }
+  }
+
+  const dispatch = (action) => {
+    state = reducer(state, action)
+    listeners.forEach((listener) => listener())
   }
 
   return {
     getState,
-    subscribe
-  };
+    subscribe,
+    dispatch,
+  }
+}
+
+// App Code
+function todos (state = [], action) {
+  if (action.type === 'ADD_TODO') {
+    return state.concat([action.todo])
+  }
+
+  return state
 }
